68118a3cefea3824e30b6b2f57954915ce676614
kindrix@gmail.com <kindrix@gmail.com>
2018-07-13 15:46:44 +1000

    Initial Commit

---
+++ ImageCropperWithAutoLayoutConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageCropperWithAutoLayoutConstraints
+//
+//  Created by Kinzang Chhogyal on 13/7/18.
+//  Copyright © 2018 Kinzang Chhogyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
